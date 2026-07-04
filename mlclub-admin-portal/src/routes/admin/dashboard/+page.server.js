39e8c977b4b4665e4165e08c1c3cecf88cd2fb82
Prachurya <prachuryabarman60@gmail.com>
2026-07-04 18:19:56 +0530

    final change

--- mlclub-admin-portal/src/routes/admin/dashboard/+page.server.js
+++ mlclub-admin-portal/src/routes/admin/dashboard/+page.server.js
@@ -2,11 +2,11 @@ import { connectDB } from "$lib/server/db.js";
 import Member from "$lib/server/models/Member.js";
 import Project from "$lib/server/models/Projects";
 import Blog from "$lib/server/models/Blogs";
-
+import mongoose from "mongoose";
 export async function load() {
 
 	await connectDB();
-
+	console.log("Database:", mongoose.connection.name);
 	const [
 		members,
         projects,
